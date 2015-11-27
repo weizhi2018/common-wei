01a027379f74660629c5e11811ef3d53cc6088fa
weizhi2018 <weizs_999@163.com>
2015-11-27 23:24:10 +0800

    最大公约数算法

---
+++ common-algorithm/src/main/java/com/weizhi/common/math/GCDivisor.java
@@ -0,0 +1,14 @@
+package com.weizhi.common.math;
+
+/**
+ * ProjectName:common-wei
+ * Decription:
+ * Created By:weizhi
+ * Created Date:2015/11/27
+ * Updated By:
+ * Updated Date:
+ * Copyright@2014-2015 weizhi2018
+ * Version:V1.0
+ */
+public class GCDivisor {
+}
