bcf678e0dfb23f79df00f937c437b6847e049974
weizhi2018 <weizs_999@163.com>
2015-12-02 21:23:13 +0800

    kafka Demo

---
+++ common-mqueue/src/test/java/com/weizhi/common/kafka/KafkaConsumerTest.java
@@ -0,0 +1,14 @@
+package com.weizhi.common.kafka;
+
+/**
+ * ProjectName:common-wei
+ * Decription:
+ * Created By:weizhi
+ * Created Date:2015/12/2
+ * Updated By:
+ * Updated Date:
+ * Copyright@2014-2015 weizhi2018
+ * Version:V1.0
+ */
+public class KafkaConsumerTest {
+}
